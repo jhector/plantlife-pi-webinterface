c204896c90470a20b892bc8effd3a6071dfb3120
Julian Hector <julian.r.hector@gmail.com>
2017-06-28 23:19:31 +0200

    Add database setup file

---
+++ database_setup.sql
@@ -0,0 +1,17 @@
+-- Create our database
+CREATE DATABASE IF NOT EXISTS plantlife;
+
+-- Create a database user
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'user';
+
+-- Grant the user access to the desired database
+GRANT ALL ON `plantlife`.* TO 'user'@'localhost' IDENTIFIED BY 'user';
+
+-- Create table for storing user infos
+CREATE TABLE IF NOT EXISTS user (userid INT, name VARCHAR(128), password VARCHAR(128), admin INT);
+
+-- Create default user admin:admin
+INSERT INTO user VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1);
+
+-- Create a table for storing the sensor data
+-- CREATE TABLE IF NOT EXISTS user (userid INT, name VARCHAR(128), password VARCHAR(128), admin INT);
