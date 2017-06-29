565a0723def5b94d0b217284446eca4120912ba1
Julian Hector <julian.r.hector@gmail.com>
2017-06-30 00:17:53 +0200

    Replace tabs with spaces

--- include/Database.php
+++ include/Database.php
@@ -8,7 +8,7 @@ class Database {
         $this->conn = mysqli_connect($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
 
         if(mysqli_connect_errno())
-	    throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
+            throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
     }
 
     public function exists($table, $condition) {
