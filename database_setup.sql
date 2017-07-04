bedaeaf4978a24383de9854178e9374ff2bf7bf2
Julian Hector <julian.r.hector@gmail.com>
2017-07-04 18:38:41 +0200

    Add tables for sensor type and sensor data

--- database_setup.sql
+++ database_setup.sql
@@ -8,9 +8,36 @@ CREATE USER 'user'@'localhost' IDENTIFIED BY 'user';
 GRANT ALL ON `plantlife`.* TO 'user'@'localhost' IDENTIFIED BY 'user';
 
 -- Create table for storing user infos
-CREATE TABLE IF NOT EXISTS user (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userid INT, name VARCHAR(128), password VARCHAR(128), admin INT);
+CREATE TABLE IF NOT EXISTS user (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userid INT,
+	name VARCHAR(128),
+	password VARCHAR(128),
+	admin INT
+);
 
--- Create default user admin:admin
-INSERT INTO user(userid, name, password, admin) VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1);
+-- Create table that contains all available sensor types
+CREATE TABLE IF NOT EXISTS sensortype (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(128)
+);
 
--- Create a table for storing the sensor data
+-- Create table for storing the sensor data
+CREATE TABLE IF NOT EXISTS sensordata (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	typeid INT,
+	value DECIMAL,
+	date DATETIME,
+	FOREIGN KEY (typeid) REFERENCES sensortype(id)
+);
+
+-- Create default admin user admin:admin (sha256("admin"))
+INSERT INTO user(userid, name, password, admin) VALUES
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1);
+
+-- Fill the sensortype table
+INSERT INTO sensortype(name) VALUES
+('moisture'),
+('humidity'),
+('temperature'),
+('light');
